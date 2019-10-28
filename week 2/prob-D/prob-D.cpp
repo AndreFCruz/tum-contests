9d8ac2e96e1d77013ee910062766d32b89c00635
AndreFCruz <andrecruz97@gmail.com>
2019-10-28 23:12:08 +0100

    actually correct prob-D

--- week 2/prob-D/prob-D.cpp
+++ week 2/prob-D/prob-D.cpp
@@ -7,12 +7,21 @@
 
 using namespace std;
 
-struct Station {
-    int id;     // Station's number
-    int u;      // Station's room range start
-    int v;      // Station's room range end
+typedef uint32_t uint;
 
-    Station(int id, int u, int v) : id(id), u(u), v(v) {}
+struct Station {
+    uint id;     // Station's number
+    uint u;      // Station's room range start
+    uint v;      // Station's room range end
+
+    Station(uint id, uint u, uint v) : id(id), u(u), v(v) {}
+};
+
+struct StationMinCmp {
+    bool operator()(const Station& s1, const Station& s2) const {
+        if (s1.u != s2.u) return s1.u < s2.u;
+        else return s1.id < s2.id;
+    }
 };
 
 struct StationIdCmp {
@@ -20,88 +29,80 @@ struct StationIdCmp {
 };
 
 class TestCase {
-    vector<Station> stations; // Maps station id to its interval
-    vector<int> friends_lines;   // maps i-th friend to respective room's line number
-    vector<int> friends_rooms;   // answer to the problem (friends' room numbers)
+    set<Station, StationMinCmp> stations;   // Ordered set of Stations
+    vector<uint> friends_lines;
+    vector<uint> friends_rooms;
 
 public:
 
     explicit TestCase(istream &in) {
-        int n_stations, n_friends;
+        uint n_stations, n_friends;
         in >> n_stations >> n_friends;
 
-        int n1, n2;
-        for (int i = 0; i < n_stations; ++i) {
+        uint n1, n2;
+        for (uint i = 0; i < n_stations; ++i) {
             in >> n1 >> n2;
-            stations.push_back(Station(i + 1, n1, n2));
+            stations.emplace(Station(i + 1, n1, n2));
         }
-        // Sort stations by their starting room number
-        sort(stations.begin(), stations.end(), [](const Station& s1, const Station& s2) {
-            if (s1.u != s2.u) return s1.u < s2.u;
-            else return s1.id < s2.id;
-        });
 
-        friends_lines = vector<int>(n_friends);
-        for (int i = 0; i < n_friends; ++i) {
+        friends_lines = vector<uint>(n_friends);
+        for (uint i = 0; i < n_friends; ++i) {
             in >> n1;
             friends_lines[i] = n1;
         }
 
-        friends_rooms = vector<int>(n_friends);
+        friends_rooms = vector<uint>(n_friends);
+    }
+
+    uint find_min() const {
+        return stations.begin()->u;
+    }
+
+    uint find_max() const {
+        uint max = 1;  // Lower limit to room number
+        for (const auto & s : stations) {
+            if (s.v > max)
+                max = s.v;
+        }
+        return max;
+    }
+
+    uint count_before(uint station) const {
+        uint before = 0;
+        for (const auto & s : stations) {
+            if (s.u <= station and station <= s.v)
+                before += (station - s.u);
+            else if (s.v < station) {
+                before += s.v - s.u + 1;
+            }
+        }
+
+        return before;
     }
 
     void solve() {
-        // Friends sorted by their line numbers
-        multimap<int, int> lines_friends;  // inverse relation from friends_lines (this maps lines to friends)
-        for (size_t i = 0; i < friends_lines.size(); ++i)
-            lines_friends.insert(make_pair(friends_lines[i], i));
+        uint min = find_min(), max = find_max();
+        for (size_t i = 0; i < friends_lines.size(); ++i) {
+            uint f_line = friends_lines[i];
+            uint mid, low = min, high = max;
+            uint min_maybe, max_maybe;
 
-        auto begin_it = lines_friends.begin(), end_it = lines_friends.end();
-        int current_line = 1, current_room = stations[0].u;
-        set<Station, StationIdCmp> active_stations;
-        // Populate active_stations
-        auto next_station = stations.begin();
-        while (next_station->u == stations[0].u) {
-            active_stations.insert(*next_station);
-            ++next_station;
-        }
+            while (high > low) {
+                mid = (high + low) / 2;
+                min_maybe = count_before(mid) + 1;
+                max_maybe = count_before(mid + 1);
 
-        auto station_it = active_stations.begin();
-        while (begin_it != end_it) {
-            int f_line = begin_it->first, f_idx = begin_it->second;
-
-            // Advance lines until this friend's line is reached, then record room number
-            while (f_line > current_line) {
-                current_line += 1;
-                ++station_it;
-
-                if (station_it == active_stations.end()) {
-                    // Progress to next room, and check which stations are now active
-                    current_room += 1;
-
-                    // Delete stations ending on this room
-                    for (auto it = active_stations.begin(); it != active_stations.end(); ) {
-                        if (it->v < current_room)
-                            it = active_stations.erase(it);
-                        else ++it;
-                    }
-
-                    // If no active stations, get next room
-                    if (active_stations.empty())
-                        current_room = next_station->u;
-
-                    // Fetch stations that are starting at room current_room
-                    while (next_station->u == current_room) {
-                        active_stations.insert(*next_station);
-                        ++next_station;
-                    }
-
-                    station_it = active_stations.begin();
+                if (min_maybe <= f_line and f_line <= max_maybe) {
+                    high = mid;
+                    break;
+                } else if (f_line < min_maybe) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
                 }
             }
 
-            friends_rooms[f_idx] = current_room;
-            ++begin_it;
+            friends_rooms[i] = high;
         }
     }
 
