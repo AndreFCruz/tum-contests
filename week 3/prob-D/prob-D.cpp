9237c1d8a8f42dfa82de3ab63ee20090c2476c7f
AndreFCruz <andrecruz97@gmail.com>
2019-11-01 21:59:18 +0100

    prob-D working

--- week 3/prob-D/prob-D.cpp
+++ week 3/prob-D/prob-D.cpp
@@ -29,50 +29,62 @@ public:
     }
 
     /**
-     * Finds the maximum distance from anywhere to the given source node.
+     * Finds the maximum distance from anywhere to the given source node (through breadth-first search).
      * @param src the source node from which to count the distance.
-     * @return the maximum distance betweent the src node and any other node.
+     * @param parent a vector representing the parent of each node in this traversal.
+     * @return a pair, representing the node which is farthest away from the src node, and its distance.
+     *  <uint farthest_node, uint respective_distance>
      */
-    uint max_distance(uint src) {
+    pair<uint, uint> max_distance_bfs(uint src, vector<uint>& parent) {
         vector<bool> discovered(num_vertices, false);
 
         uint max_dist = 0;
+        uint farthest_node = src;
         queue<pair<uint, uint>> q;  // queue of pairs <node_id, dist_to_source>
         q.push(make_pair(src, 0)); discovered[src] = true;
         while (! q.empty()) {
             uint node = q.front().first, dist = q.front().second;
             q.pop();
-            if (dist > max_dist) max_dist = dist;
+            if (dist > max_dist) {
+                max_dist = dist;
+                farthest_node = node;
+            }
 
             for (auto it = edges.equal_range(node); it.first != it.second; ++it.first) {
                 uint dest = it.first->second;
                 if (! discovered[dest]) {
+                    parent[dest] = node;
                     discovered[dest] = true;
                     q.push(make_pair(dest, dist + 1));
                 }
             }
         }
 
-        return max_dist;
+        return make_pair(farthest_node, max_dist);
     }
 
     uint solve() {
-        uint min_max_distance = numeric_limits<uint>::max();
-        uint best_node = numeric_limits<uint>::max();
+        uint src = 0;   // arbitrary source node
+        vector<uint> parent(num_vertices);
+        auto t = max_distance_bfs(src, parent);
+        auto u = max_distance_bfs(t.first, parent);
 
-        for (uint i = 0; i < num_vertices; ++i) {
-            uint max_dist = max_distance(i);
-            if (min_max_distance > max_dist) {
-                min_max_distance = max_dist;
-                best_node = i;
-            }
-        }
+        // path <u, t> is the tree's diameter
+        uint diameter_length = u.second;
 
-        return best_node;
+        // Rebuild path from t to u, the center of the tree is the middle vertex
+        uint count = 0;
+        uint center = u.first;
+        while (count++ < diameter_length / 2)
+            center = parent[center];
+
+        return center;
     }
 };
 
 int main() {
+    std::ios_base::sync_with_stdio(false);
+
     uint num_cases;
     cin >> num_cases;
     for (uint i = 0; i < num_cases; ++i) {
