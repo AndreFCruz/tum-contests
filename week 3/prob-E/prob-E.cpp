bb1fc960e79a656b758ea2e0a26ab7c2c59dc055
AndreFCruz <andrecruz97@gmail.com>
2019-11-03 12:08:37 +0100

    different attempt at prob-E

--- week 3/prob-E/prob-E.cpp
+++ week 3/prob-E/prob-E.cpp
@@ -12,6 +12,63 @@ using namespace std;
 
 typedef unsigned int uint;
 
+class UnionFind {
+private:
+    size_t n;
+    vector<uint> parent;
+    vector<uint> size;
+
+public:
+    UnionFind(size_t n) : n(n) {
+        this->size = vector<uint>(n, 1);
+        this->parent = vector<uint>(n);
+
+        uint i = 0;
+        generate(parent.begin(), parent.end(), [&]() {
+            return i++;
+        });
+
+    }
+
+    uint find(uint a) {
+        // Find root
+        uint root = a;
+        while (parent[root] != root)
+            root = parent[root];
+
+        // Compress path
+        uint current = a, next_elem;
+        while (current != root) {
+            next_elem = parent[current];
+            parent[current] = root;
+            current = next_elem;
+        }
+
+        return root;
+    }
+
+    uint merge(uint a, uint b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) // a and b are already merged
+            return a;
+
+        // Weighted union: update smaller component
+        uint a_size = size[a], b_size = size[b];
+        if (a_size < b_size)
+            swap(a, b);
+
+        parent[b] = a;
+        // Update size accordingly
+        size[a] = a_size + b_size;
+        return a;
+    }
+
+    uint size_of(uint a) {
+        return size[find(a)];
+    }
+};
+
 struct Hallway {
     uint a, b;          // the hallway's connecting rooms
     uint water_level;   // to use this hallway, the water must be at this level
@@ -51,58 +108,41 @@ public:
 
     string solve(uint start_node = 1) {
 
-        vector<bool> visited(num_rooms + 1, false);
-        set<uint> nodes;
-        nodes.insert(start_node);
-        visited[start_node] = true;
-        uint curr_water_lv = init_water_lv, last_used_water_lv = init_water_lv;
+        UnionFind uf(num_rooms + 1);
+        uint curr_water_lv = init_water_lv;
 
-        // While there are reachable nodes
-        while (! nodes.empty()) {
-            uint n = *nodes.begin();
-            nodes.erase(nodes.begin());
+        // Unite all accessible nodes in subsets
+        auto h_it = hallways.begin();
+        for ( ; h_it != hallways.end() and h_it->water_level >= curr_water_lv; ++h_it)
+            uf.merge(h_it->a, h_it->b);
 
-            // If it's a control room, lower water level
-            auto it = control_rooms.find(n);
-            if (it != control_rooms.end()) {
-                curr_water_lv = min(curr_water_lv, it->second);
+        // While there are inaccessible rooms
+        while (uf.size_of(uf.find(start_node)) < num_rooms) {
+            uint objective_water_lv = h_it->water_level;
+            vector<uint> accessible_rooms;
+            for (uint n = 1; n <= num_rooms; ++n) {
+                if (uf.find(start_node) == uf.find(n))
+                    accessible_rooms.push_back(n);
             }
 
-            // Access all possible hallways
-            auto hallway_it = hallways.begin();
-            while (hallway_it != hallways.end() and hallway_it->water_level >= curr_water_lv) {
-                bool used = false;
-                if (visited[hallway_it->a] and !visited[hallway_it->b]) {
-                    nodes.insert(hallway_it->b);
-                    visited[hallway_it->b] = true;
-                    used = true;
-                }
-                else if (!visited[hallway_it->a] and visited[hallway_it->b]) {
-                    nodes.insert(hallway_it->a);
-                    visited[hallway_it->a] = true;
-                    used = true;
-                }
-                else if (visited[hallway_it->a] and visited[hallway_it->b]) {
-                    hallway_it = hallways.erase(hallway_it);
-                    continue;
-                }
-
-                // If this hallway was used
-                if (used) {
-//                    cout << "Using hallway <" << hallway_it->a << "; " << hallway_it->b << "> at " << hallway_it->water_level << endl; // TODO delete
-                    last_used_water_lv = min(last_used_water_lv, hallway_it->water_level);
-                    hallway_it = hallways.erase(hallway_it);
-                } else {
-                    ++hallway_it;
+            bool progress = false;
+            for (uint room : accessible_rooms) {
+                auto r_it = control_rooms.find(room);
+                if (r_it != control_rooms.end() and r_it->second <= objective_water_lv) {
+                    curr_water_lv = objective_water_lv;
+                    progress = true;
                 }
             }
+
+            if (! progress)
+                return "impossible";
+
+            // Unite all accessible nodes
+            for (; h_it != hallways.end() and h_it->water_level >= curr_water_lv; ++h_it)
+                uf.merge(h_it->a, h_it->b);
         }
 
-        // Check if all nodes were visited, and return appropriately
-        for (uint i = 1; i < visited.size(); ++i)
-            if (! visited[i]) return "impossible";
-
-        return to_string(last_used_water_lv);
+        return to_string(curr_water_lv);
     }
 };
 
