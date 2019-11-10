b608aaddfe6f885db3104b65bd86c494a7e277a8
AndreFCruz <andrecruz97@gmail.com>
2019-11-10 22:45:31 +0100

    prob-E

--- week 4/prob-E/prob-E.cpp
+++ week 4/prob-E/prob-E.cpp
@@ -3,22 +3,108 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <queue>
+#include <limits>
 
 using namespace std;
 
+struct Edge {
+    uint origin, dest, cost;
+};
+
 class TestCase {
-    uint n_vertices;
-    multimap<uint, uint> edges;
-    vector<uint> costs;
+    uint n_junctions;
+    multimap<uint, Edge> edges;
+    vector<uint> daily_shortest_path;
 
 public:
     explicit TestCase(istream& in) {
+        uint n_paths;   // number of streets and footpaths
+        uint n_junctions_daily_path; // number of vertices in the shortest path
 
+        in >> n_junctions >> n_paths >> n_junctions_daily_path;
+
+        daily_shortest_path = vector<uint>(n_junctions_daily_path);
+        for (uint i = 0; i < n_junctions_daily_path; ++i) {
+            in >> daily_shortest_path[i];
+        }
+
+        for (uint i = 0; i < n_paths; ++i) {
+            Edge e;
+            in >> e.origin >> e.dest >> e.cost;
+            edges.insert(make_pair(e.origin, e));
+        }
+    }
+
+    /**
+     * Executes Dijkstra's shortest-path algorithm from the given source node,
+     *  and returns distances to all nodes in the graph.
+     * @param edges edges of the graph
+     * @param num_vertices total number of vertices
+     * @param start_node start node from which to count distances
+     * @return matrix of possible predecessors for each vertex
+     */
+    static vector<vector<uint>> dijkstra(const multimap<uint, Edge>& edges, uint num_vertices, uint start_node) {
+        vector<uint> dist(num_vertices, numeric_limits<uint>::max());
+        vector<bool> visited(num_vertices, false);
+        vector<vector<uint>> predecessor(num_vertices, vector<uint>());
+
+        // NOTE this would be more efficient with a Fibonacci Heap
+        typedef pair<uint, uint> pi;
+        priority_queue<pi, vector<pi>, greater<pi>> priorityQueue; // priority queue of elements <distance, node>
+        priorityQueue.push(make_pair(0, start_node)); // start_node is at distance 0
+        dist[start_node] = 0;
+        while (! priorityQueue.empty()) {
+            auto p = priorityQueue.top(); priorityQueue.pop();
+            uint node = p.second;
+            visited[node] = true;
+
+            // Check edges to other cities
+            for (auto it = edges.equal_range(node); it.first != it.second; ++it.first) {
+                Edge e = it.first->second;
+                uint distToNeighbor = p.first + e.cost;
+                if (!visited[e.dest] and distToNeighbor <= dist[e.dest]) {
+                    if (distToNeighbor == dist[e.dest]) {
+                        predecessor[e.dest].push_back(e.origin);
+                    } else {
+                        predecessor[e.dest] = vector<uint>{e.origin};
+                        dist[e.dest] = distToNeighbor;
+                        priorityQueue.push(make_pair(distToNeighbor, e.dest));
+                        // NOTE We should delete the previous pair containing this node
+                    }
+                }
+            }
+        }
+
+        return predecessor;
     }
 
     string solve() {
+//        cout << "EDGES:\n";
+//        for (const auto& p : edges) // TODO delete
+//            cout << p.second.origin << " -> " << p.second.dest << " with w=" << p.second.cost << endl;
 
-        return "impossibruuuu";
+        // Check if any edge in the path can be changed
+        for (uint i = 0; i < daily_shortest_path.size() - 1; ++i) {
+            uint node = daily_shortest_path[i], next_node = daily_shortest_path[i+1];
+
+            uint min_edge = numeric_limits<uint>::max();
+            for (auto it = edges.equal_range(node); it.first != it.second; ++it.first) {
+                Edge e = it.first->second;
+                if (e.dest == next_node) {
+                    if (e.cost < min_edge) min_edge = e.cost;
+                    else if (e.cost == min_edge) return "yes";
+                }
+            }
+        }
+
+        // Check if any node in the path can be changed
+        auto predecessors = dijkstra(edges, this->n_junctions + 1, 1);
+        for (uint node : daily_shortest_path) {
+            if (predecessors[node].size() > 1) return "yes";
+        }
+
+        return "no";
     }
 };
 
