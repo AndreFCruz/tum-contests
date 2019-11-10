69806ac92e2bdca394b8a34b83e57b2f25117749
AndreFCruz <andrecruz97@gmail.com>
2019-11-10 18:36:12 +0100

    prob-C

--- week 4/prob-C/prob-C.cpp
+++ week 4/prob-C/prob-C.cpp
@@ -13,19 +13,12 @@ using namespace std;
 struct Edge {
     uint origin, dest, cost;
 
-    Edge reverse() {
+    Edge reverse() const {
         Edge reverse;
         reverse.origin = this->dest;
         reverse.dest = this->origin;
         reverse.cost = this->cost;
         return reverse;
-    }
-};
-
-struct EdgeCmp {
-    bool operator()(const Edge& e1, const Edge& e2) {
-        if (e1.cost != e2.cost) return e1.cost < e2.cost;
-        return e1.dest != e2.dest ? e1.dest < e2.dest : e1.origin < e2.origin;
     }
 };
 
@@ -37,7 +30,11 @@ class TestCase {
     uint end_node;      // b
 
     // Roads between cities
-    multimap<uint, Edge, EdgeCmp> edges;
+    multimap<uint, Edge> edges;
+
+    // Mapping from market's true node to fictional node (and reverse)
+//    unordered_map<uint, uint> true_to_fictional;
+//    unordered_map<uint, uint> fictional_to_true;
 
     // Supermarkets per city, there may be multiple per city but we only care about the fastest
     unordered_map<uint, uint> markets;   // markets[market_node] = market_time
@@ -75,6 +72,43 @@ public:
         return string(formatted);
     }
 
+    /**
+     * Executes Dijkstra's shortest-path algorithm from the given source node,
+     *  and returns distances to all nodes in the graph.
+     * @param edges edges of the graph
+     * @param num_vertices total number of vertices
+     * @param start_node start node from which to count distances
+     * @return vector of distances for every node in the graph
+     */
+    static vector<uint> dijkstra(const multimap<uint, Edge>& edges, uint num_vertices, uint start_node) {
+        vector<uint> dist(num_vertices, numeric_limits<uint>::max());
+        vector<bool> visited(num_vertices, false);
+
+        // NOTE this would be more efficient with a Fibonacci Heap
+        typedef pair<uint, uint> pi;
+        priority_queue<pi, vector<pi>, greater<pi>> priorityQueue; // priority queue of elements <distance, node>
+        priorityQueue.push(make_pair(0, start_node)); // starts at node 1 with distance 0
+        dist[start_node] = 0;
+        while (! priorityQueue.empty()) {
+            auto p = priorityQueue.top(); priorityQueue.pop();
+            uint node = p.second;
+            visited[node] = true;
+
+            // Check edges to other cities
+            for (auto it = edges.equal_range(node); it.first != it.second; ++it.first) {
+                Edge e = it.first->second;
+                uint distToNeighbor = p.first + e.cost;
+                if (!visited[e.dest] and distToNeighbor < dist[e.dest]) {
+                    dist[e.dest] = distToNeighbor;
+                    priorityQueue.push(make_pair(distToNeighbor, e.dest));
+                    // NOTE We should delete the previous pair containing this node
+                }
+            }
+        }
+
+        return dist;
+    }
+
     string solve() {
         // Do Dijkstra shortest-path
         // maintain 2 distance vectors: one as normal, and another for paths which have gone through a supermarket
@@ -90,7 +124,41 @@ public:
         //  number of supermarket many times"
         // (brute-force but there are at most 10000 supermarkets)
 
-        return "impossibruu";
+        // New set of edges, containing fictional edges to/from super-markets
+//        for (const auto& p : markets) {
+//            n_fictional_nodes += 1;
+//
+//            Edge e;
+//            e.origin = p.first;             // true market node
+//            e.dest = n_fictional_nodes;     // fictional market node
+//            e.cost = p.second;              // cost to visit market
+//            extended_edges.insert(make_pair(e.origin, e));
+//
+//            Edge reverse = e.reverse();     // Edge from market node to city node, with cost 0
+//            reverse.cost = 0;
+//            extended_edges.insert(make_pair(e.dest, reverse));
+//        }
+
+        // Reverse edges for dijkstra from destination node
+        multimap<uint, Edge> reverse_edges;
+        for (const auto& p : edges)
+            reverse_edges.insert(make_pair(p.second.dest, p.second.reverse()));
+
+        // Distances from source node
+        vector<uint> dist_from_src = TestCase::dijkstra(edges, n_cities + 1, start_node);
+
+        // Distances from destination node ** with all edges reversed **
+        vector<uint> dist_from_dest = TestCase::dijkstra(reverse_edges, n_cities + 1, end_node);
+
+        // Sum distances to and from all super-markets' fictional nodes
+        uint min_dist = numeric_limits<uint>::max();
+        for (const auto& market : this->markets) {
+            uint node = market.first, queue_time = market.second;
+            if (dist_from_src[node] != numeric_limits<uint>::max() and dist_from_dest[node] != numeric_limits<uint>::max())
+                min_dist = min(min_dist, dist_from_src[node] + dist_from_dest[node] + queue_time);
+        }
+
+        return min_dist == numeric_limits<uint>::max() ? "impossible" : minutes_to_time_format(min_dist);
     }
 };
 
