436a3618501bc0a32b79991b132e47824d3aea97
AndreFCruz <andrecruz97@gmail.com>
2019-11-15 19:23:18 +0100

    prob-E first solution

--- week 5/prob-E/prob-E.cpp
+++ week 5/prob-E/prob-E.cpp
@@ -138,40 +138,114 @@ template <class T> struct PushRelabel {
 };
 
 class TestCase {
-    int n_vertices;
-    int n_edges;
+    int grid_size;
+    int n_riders;
+    int n_nights;
 
-    vector<int> wins;
-    multimap<int, int> connections;
-
-    int source, sink;
+    vector<vector<int>> grid_height;
+    vector<pair<int, int>> rider_pos;
+    vector<int> nightly_snow_height;
 
 public:
     explicit TestCase(istream& in) {
-        in >> n_vertices >> n_edges;
-        this->source = 1;
-        this->sink = this->n_vertices;
+        in >> grid_size >> n_riders >> n_nights;
 
-        this->wins = vector<int>(this->n_vertices + 1);
-        for (int i = 1; i <= this->n_vertices; ++i)
-            in >> this->wins[i];
-
-        int a, b;
-        for (int i = 0; i < n_edges; ++i) {
-            in >> a >> b;
-            if (a > b) swap(a, b);
-            connections.insert(make_pair(a, b));
+        this->grid_height = vector<vector<int>>(grid_size, vector<int>(grid_size));
+        for (int row = 0; row < grid_size; ++row) {
+            for (int col = 0; col < grid_size; ++col) {
+                in >> grid_height[row][col];
+            }
         }
 
-        this->source = 0;
-        this->sink = this->n_vertices + 1;
-        this->n_vertices += 2;
+        this->rider_pos = vector<pair<int, int>>(n_riders);
+        for (int i = 0; i < n_riders; ++i) {
+            in >> rider_pos[i].first >> rider_pos[i].second;
+        }
+
+        this->nightly_snow_height = vector<int>(n_nights);
+        for (int i = 0; i < n_nights; ++i) {
+            in >> nightly_snow_height[i];
+        }
     }
 
+    /**
+     * Translates node's features to its vertex index/id.
+     */
+    int to_vertex_id(int row, int col, int day) {
+        return col + row * grid_size + day * (grid_size * grid_size);
+    }
+
+    void add_grid_edges(PushRelabel<int>& pr, int day) {
+        for (int row = 0; row < this->grid_size; ++row) {
+            for (int col = 0; col < this->grid_size; ++col) {
+                // Edge downwards
+                if (row < this->grid_size - 1) {
+                    int a = to_vertex_id(row, col, day);
+                    int b = to_vertex_id(row + 1, col, day);
+                    pr.AddEdge(a, b, 1);
+                    pr.AddEdge(b, a, 1);
+                }
+
+                // Edge rightwards
+                if (col < this->grid_size - 1) {
+                    int a = to_vertex_id(row, col, day);
+                    int b = to_vertex_id(row, col + 1, day);
+                    pr.AddEdge(a, b, 1);
+                    pr.AddEdge(b, a, 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Adds the edges between corresponding nodes of different days, depending on
+     *  whether that node is survivable.
+     * @param pr PushRelabel instance to be modified.
+     * @param night index of current night in range [0, n_nights[
+     */
+    void add_night_edges(PushRelabel<int>& pr, int night) {
+        for (int row = 0; row < grid_size; ++row) {
+            for (int col = 0; col < grid_size; ++col) {
+
+                // Is node survivable?
+                if (grid_height[row][col] > nightly_snow_height[night]) {
+                    pr.AddEdge(to_vertex_id(row, col, night), to_vertex_id(row, col, night+1), 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Constructs a flow network representing the "Goat Riders" problem.
+     * - each day is a repetition of the original grid/network in a different dimension;
+     * - different days are joined through a 1-capacity edge if rider could survive that night.
+     */
     string solve() {
+        int n_vertices = 3 + (grid_size * grid_size) * (n_nights + 1);
+        PushRelabel<int> pr(n_vertices);
 
+        int pre_source = n_vertices - 3, source = n_vertices - 2, sink = n_vertices - 1;
+        // Add edge from pre_source to source (to control maximum outgoing flow from source)
+        pr.AddEdge(pre_source, source, n_riders);
 
-        return "impossibruuu";
+        // Add edges from source to first day's nodes
+        for (int i = 0; i < grid_size * grid_size; ++i)
+            pr.AddEdge(source, i, 1);
+
+        // Add edges from lat day's nodes to sink
+        for (int i = grid_size * grid_size * n_nights; i < grid_size * grid_size * (n_nights + 1); ++i)
+            pr.AddEdge(i, sink, 1);
+
+        // For every day, add edges between neighbouring grid nodes
+        for (int i = 0; i < n_nights + 1; ++i)
+            add_grid_edges(pr, i);
+
+        // For every night, add edges between analogous survivable nodes
+        for (int i = 0; i < n_nights; ++i) {
+            add_night_edges(pr, i);
+        }
+
+        return to_string(pr.GetMaxFlow(pre_source, sink));
     }
 };
 
