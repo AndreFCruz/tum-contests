5cc06245be0261788ff15fe7d03713f85172dce2
AndreFCruz <andrecruz97@gmail.com>
2019-12-15 09:39:53 +0100

    prob-A solved

--- week 9/prob-A/prob-A.cpp
+++ week 9/prob-A/prob-A.cpp
@@ -2,17 +2,52 @@
 #include <vector>
 #include <cassert>
 #include <algorithm>
+#include <cmath>
 #include <map>
 #include <limits>
 #include <queue>
 
+typedef unsigned int uint;
+
 using namespace std;
 
+// Largest common divisor between a list of numbers
 
 class TestCase {
+    vector<uint> nums;
+
 public:
     explicit TestCase(istream& in) {
+        int n_nums;
+        in >> n_nums;
 
+        nums.resize(n_nums);
+        for (int i = 0; i < n_nums; ++i)
+            in >> nums[i];
+    }
+
+    static uint lgd(vector<uint>& nums) {
+        uint min_num = *min(nums.begin(), nums.end());
+
+        uint max_div = 1;
+        for (uint i = 1; i <= min_num; ++i) {
+            bool all_dividable = true;
+            for (uint n : nums) {
+                if (n % i != 0) {
+                    all_dividable = false;
+                    break;
+                }
+            }
+
+            if (all_dividable)
+                max_div = i;
+        }
+
+        return max_div;
+    }
+
+    string solve() {
+        return to_string(lgd(nums));
     }
 };
 
