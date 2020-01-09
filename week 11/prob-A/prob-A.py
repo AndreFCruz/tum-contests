9342f52b91cc8ceef76ce51d7161843cf87f7cca
AndreFCruz <andrecruz97@gmail.com>
2020-01-09 22:27:16 +0100

    week 11 prob A

--- week 11/prob-A/prob-A.py
+++ week 11/prob-A/prob-A.py
@@ -3,6 +3,7 @@ Week 11 - Problem A - Contact List
 """
 
 import sys
+from queue import SimpleQueue
 
 
 class TrieNode:
@@ -10,27 +11,61 @@ class TrieNode:
     self.value = value
     self.is_word_end = is_word_end
     self.children = list()
+  
+  def to_string(self, accumulated=''):
+    ret = list()
+    if self.is_word_end:
+      ret.append(accumulated + self.value)
 
-def trie_search(root: TrieNode, value):
-  trie_search_aux(root, value, '')
+    for c in self.children:
+      ret.extend(c.to_string(accumulated + self.value))
+    return ret
 
-## NOTE This recursive implementation can be replaced with Iteration + Queue + backtracking
-def trie_search_aux(root: TrieNode, value, accumulated):
-  if value == accumulated:
-    return root
-
-  for child in root.children:
-    res = trie_search_aux(child, value, accumulated + child.value)
-    if res is not None:
-      return res
-
-  return None
 
 def trie_insert(root: TrieNode, value):
-  pass
+  '''
+  Inserts value in the given Trie,
+  returns True if possible or False if repeated
+  '''
+  node = root
+  for i in range(len(value)):
+    new_node = None
+    for child in node.children:
+      if child.value == value[i]:
+        new_node = child
+        break
+
+    if new_node is None:
+      new_node = TrieNode(value[i])
+      node.children.append(new_node)
+    node = new_node
+
+  if node.is_word_end: return False # repeated word
+  node.is_word_end = True
+  return True # successful insertion
+
 
 def solve_test_case():
+  n = int(input())
+  contacts = [sys.stdin.readline().strip() for _ in range(n)]
 
+  trie_root = TrieNode('')
+  for c in contacts:
+    trie_insert(trie_root, c)
+  
+  # Count number of intermediate nodes corresponding to word endings
+  count = 0
+  q = SimpleQueue()
+  q.put_nowait(trie_root)
+  while not q.empty():
+    node = q.get_nowait()
+    for c in node.children:
+      q.put_nowait(c)
+    
+    if len(node.children) > 0 and node.is_word_end:
+      count += 1
+
+  return count
 
 
 if __name__ == '__main__':
